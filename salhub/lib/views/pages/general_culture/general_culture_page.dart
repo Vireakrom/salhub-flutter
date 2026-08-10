edaacb930ea3be33e882f263bfb9ed3b85687402
Vireakrom <vireakrom69@gmail.com>
2026-08-10 11:22:23 +0700

    Add example page to each folder

---
+++ salhub/lib/views/pages/general_culture/general_culture_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GeneralCulturePage extends StatelessWidget {
+  const GeneralCulturePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ salhub/lib/views/pages/sraknisaii/sraknisaii_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SraknisaiiPage extends StatelessWidget {
+  const SraknisaiiPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
