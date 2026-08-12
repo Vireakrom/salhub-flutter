3a20e12c6e8bf9f25d080d62b72fcf8800b3fec7
Malis <ml4623329@gmail.com>
2026-08-12 09:57:34 +0700

    Jomrous

--- salhub/lib/views/pages/jomrous/jomrous_page.dart
+++ salhub/lib/views/pages/jomrous/jomrous_page.dart
@@ -1,20 +1,330 @@
 import 'package:flutter/material.dart';
+import 'package:salhub/views/pages/jomrous/vegetable.dart';
 import 'package:salhub/views/widgets/back_button_widget.dart';
+
+
 
 class JomrousPage extends StatelessWidget {
   const JomrousPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+        double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
         leadingWidth: 100,
         leading: BackButtonWidget(),
-        title: Text("ភាសារចម្រុះ"),
         centerTitle: true,
       ),
-      body: Column(children: [Text("Hello")]),
+
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "ភាសាចម្រុះ",
+                  style: TextStyle(fontSize: 25, color: Color(0xFF790303)),
+                ),
+                SizedBox(width: 10,),
+                Text(
+                  "ភាសាចម្រុះ",
+                  style: TextStyle(fontSize: 25, color: Color(0xFF790303)),
+                ),
+              ],
+            ),
+            
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Placeholder()),
+                );
+              },
+              child: Center(
+              child: Container(
+                width: screenWidth*0.8,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 239, 231, 152),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Color(0xFFF7BE2D), width: 2),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                
+                    children: [
+                      Text(
+                        "បន្លែ",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                      Text(
+                        "Vegetable",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),),
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => VegetablePage()),
+                );
+              },
+              child: Container(
+                width: screenWidth*0.8,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 239, 231, 152),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Color(0xFFF7BE2D), width: 2),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                
+                    children: [
+                      Text(
+                        "ផ្លែឈើ",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                      Text(
+                        "Fruit",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Placeholder()),
+                );
+              },
+              child: Container(
+                width: screenWidth*0.8,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 239, 231, 152),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Color(0xFFF7BE2D), width: 2),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                
+                    children: [
+                      Text(
+                        "ពណ៏",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                      Text(
+                        "Colors",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Placeholder()),
+                );
+              },
+              child: Container(
+                width: screenWidth*0.8,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 239, 231, 152),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Color(0xFFF7BE2D), width: 2),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                
+                    children: [
+                      Text(
+                        "ផ្ទះ",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                      Text(
+                        "Houses",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Placeholder()),
+                );
+              },
+              child: Container(
+                width: screenWidth*0.8,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 239, 231, 152),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Color(0xFFF7BE2D), width: 2),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                
+                    children: [
+                      Text(
+                        "ភោជនីយដ្ធាន",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                      Text(
+                        "Resturants",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Placeholder()),
+                );
+              },
+              child: Container(
+                width: screenWidth*0.8,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 239, 231, 152),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Color(0xFFF7BE2D), width: 2),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                
+                    children: [
+                      Text(
+                        "ពេលវេលា",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                      Text(
+                        "Times",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Placeholder()),
+                );
+              },
+              child: Container(
+                width: screenWidth*0.8,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 239, 231, 152),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Color(0xFFF7BE2D), width: 2),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                
+                    children: [
+                      Text(
+                        "ពណ៏",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                      Text(
+                        "Colors",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF790303),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 50,),
+             
+          ],
+        ),
+      ),
     );
   }
 }
